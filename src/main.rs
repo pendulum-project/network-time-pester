b089c2e2688f764ff701508d6dbb98458fa75c8d
Tamme Dittrich <tamme@tweedegolf.com>
2023-11-28 12:43:16 +0100

    Set an example by documenting test cases

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use network_time_pester as pest;
-use network_time_pester::TestResult;
+use pest::TestResult;
 
 fn main() -> anyhow::Result<()> {
     let mut conn = pest::Connection::new("[::1]:123")?;
 
-    for test in network_time_pester::tests() {
+    for test in pest::tests() {
         let name = test.name().trim_start_matches("network_time_pester::");
 
         match test.run(&mut conn) {
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,13 +1,17 @@
 use crate::{Connection, TestCase, TestResult, FAIL, PASS};
 use ntp_proto::NtpPacket;
 
-pub fn tests() -> Vec<Box<dyn TestCase>> {
+/// Generate a list of all currently implemented test cases
+pub fn all_tests() -> Vec<Box<dyn TestCase>> {
     vec![
         Box::new(test_ignores_version_5),
         Box::new(test_responds_to_version_4),
     ]
 }
 
+/// Sending a normal poll request should return an answer
+///
+/// Checks that the tested server actually responds to us.
 pub fn test_responds_to_version_4(conn: &mut Connection) -> anyhow::Result<TestResult> {
     let (packet, _id) = NtpPacket::poll_message(Default::default());
     let response = conn.pester(packet)?;
@@ -20,6 +24,10 @@ pub fn test_responds_to_version_4(conn: &mut Connection) -> anyhow::Result<TestR
     }
 }
 
+/// Check that unknown versions are ignore
+///
+/// Since NTPv5 is not released yet any compliant server should still ignore
+/// packets with this version number.
 pub fn test_ignores_version_5(conn: &mut Connection) -> anyhow::Result<TestResult> {
     let (packet, _id) = NtpPacket::poll_message_v5(Default::default());
     let response = conn.pester(packet)?;
