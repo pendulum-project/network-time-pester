3ea910b427029b3a7db34fab2d0c6005bfd663a3
Tamme Dittrich <tamme@tweedegolf.com>
2023-11-29 10:28:30 +0100

    Expand happy test case

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@ use network_time_pester as pest;
 use pest::TestResult;
 
 fn main() -> anyhow::Result<()> {
-    let mut conn = pest::Connection::new("[::1]:123")?;
+    let server = "[::1]:123";
+    // let server = "time.windows.com:123";
 
-    for test in pest::tests() {
+    let mut conn = pest::Connection::new(server)?;
+
+    for test in pest::tests::all_tests() {
         let name = test.name().trim_start_matches("network_time_pester::");
 
         match test.run(&mut conn) {
             Ok(TestResult::Pass) => println!("✓ {name}"),
-            Ok(TestResult::Fail) => println!("❌ {name}"),
+            Ok(TestResult::Fail(msg, None)) => println!("❌ {name}\n ↳ {msg}"),
+            Ok(TestResult::Fail(msg, Some(r))) => println!("❌ {name}\n ↳ {msg}\n ↳ {r:#?}"),
             Err(e) => println!("❓ {name}:\n ↳ {e:#}"),
         }
     }
