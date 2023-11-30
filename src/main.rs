b9a4a7b658b5e0fd66b6cde17ca892128cdb8b99
marlonp <marlonpeeters@tweedegolf.com>
2023-11-30 16:48:47 +0100

    Simplified cli

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
-use std::{net::IpAddr, str::FromStr};
+use std::{net::SocketAddr, str::FromStr};
 
 use clap::Parser;
 use network_time_pester as pest;
 use pest::TestResult;
 
-const DEFAULT_SERVER: &str = "::1:123";
-const DEFAULT_PORT: u16 = 123;
-
 #[derive(Parser)]
 struct Cli {
-    #[arg(short, long)]
-    server: Option<IpAddr>,
-    #[arg(short, long)]
-    port: Option<u16>,
+    #[arg(default_value_t = SocketAddr::from_str("[::1]:123").expect("invalid socket address"))]
+    server: SocketAddr,
 }
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    
+
     // TODO: Add a cli for server(s) and timeout
-    let server = cli.server.unwrap_or(IpAddr::from_str(DEFAULT_SERVER)?);
-    let port = cli.port.unwrap_or(DEFAULT_PORT);
-    
-    let mut conn = pest::Connection::new((server, port))?;
+    let mut conn = pest::Connection::new(cli.server)?;
 
     let mut passed = 0;
     let mut failed = 0;
@@ -33,15 +25,15 @@ fn main() -> anyhow::Result<()> {
             Ok(TestResult::Pass) => {
                 passed += 1;
                 println!("✅ {name}");
-            },
+            }
             Ok(TestResult::Fail(msg, None)) => {
                 failed += 1;
                 println!("❌ {name}\n ↳ {msg}")
-            },
+            }
             Ok(TestResult::Fail(msg, Some(r))) => {
                 failed += 1;
                 println!("❌ {name}\n ↳ {msg}\n ↳ {r:#?}")
-            },
+            }
             Err(e) => println!("❓ {name}:\n ↳ {e:#}"),
         }
     }
