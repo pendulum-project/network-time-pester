660e0133e8a51164641830bb38b08c62ef42b8a3
marlonp <marlonpeeters@tweedegolf.com>
2023-11-30 13:57:39 +0100

    Better checkmark

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use pest::TestResult;
 
 fn main() -> anyhow::Result<()> {
     // TODO: Add a cli for server(s) and timeout
-    let server = "[::1]:123";
+    let server = "[::1]:1123";
     // let server = "time.windows.com:123";
     // let server = "pool.ntp.org:123";
     // let server = "ptbtime1.ptb.de:123";
@@ -14,7 +14,7 @@ fn main() -> anyhow::Result<()> {
         let name = test.name().trim_start_matches("network_time_pester::");
 
         match test.run(&mut conn) {
-            Ok(TestResult::Pass) => println!("✓ {name}"),
+            Ok(TestResult::Pass) => println!("✅ {name}"),
             Ok(TestResult::Fail(msg, None)) => println!("❌ {name}\n ↳ {msg}"),
             Ok(TestResult::Fail(msg, Some(r))) => println!("❌ {name}\n ↳ {msg}\n ↳ {r:#?}"),
             Err(e) => println!("❓ {name}:\n ↳ {e:#}"),
