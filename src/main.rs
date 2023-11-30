e91f6462a9d66a70c18063d3bc0668aa9b24488c
marlonp <marlonpeeters@tweedegolf.com>
2023-11-30 14:26:06 +0100

    Added clap and a simple test report

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,52 @@
+use std::{net::IpAddr, str::FromStr};
+
+use clap::Parser;
 use network_time_pester as pest;
 use pest::TestResult;
 
+const DEFAULT_SERVER: &str = "::1:123";
+const DEFAULT_PORT: u16 = 123;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    server: Option<IpAddr>,
+    #[arg(short, long)]
+    port: Option<u16>,
+}
+
 fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    
     // TODO: Add a cli for server(s) and timeout
-    let server = "[::1]:1123";
-    // let server = "time.windows.com:123";
-    // let server = "pool.ntp.org:123";
-    // let server = "ptbtime1.ptb.de:123";
+    let server = cli.server.unwrap_or(IpAddr::from_str(DEFAULT_SERVER)?);
+    let port = cli.port.unwrap_or(DEFAULT_PORT);
+    
+    let mut conn = pest::Connection::new((server, port))?;
 
-    let mut conn = pest::Connection::new(server)?;
-
+    let mut passed = 0;
+    let mut failed = 0;
     for test in pest::all_tests() {
         let name = test.name().trim_start_matches("network_time_pester::");
 
         match test.run(&mut conn) {
-            Ok(TestResult::Pass) => println!("✅ {name}"),
-            Ok(TestResult::Fail(msg, None)) => println!("❌ {name}\n ↳ {msg}"),
-            Ok(TestResult::Fail(msg, Some(r))) => println!("❌ {name}\n ↳ {msg}\n ↳ {r:#?}"),
+            Ok(TestResult::Pass) => {
+                passed += 1;
+                println!("✅ {name}");
+            },
+            Ok(TestResult::Fail(msg, None)) => {
+                failed += 1;
+                println!("❌ {name}\n ↳ {msg}")
+            },
+            Ok(TestResult::Fail(msg, Some(r))) => {
+                failed += 1;
+                println!("❌ {name}\n ↳ {msg}\n ↳ {r:#?}")
+            },
             Err(e) => println!("❓ {name}:\n ↳ {e:#}"),
         }
     }
 
-    // TODO: build a nice report?
+    println!("\n✅ Passed: {passed}\n❌ Failed: {failed}");
 
     Ok(())
 }
